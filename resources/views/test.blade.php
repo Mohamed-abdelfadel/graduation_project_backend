bae742efd5d73dd02e7f12b8a1e6d37cf6c0556a
Mohamed_Yasser <mohamed.yasser99999@gmail.com>
2022-11-26 16:15:29 +0200

    setting logic

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('title')
+    API's
+@endsection
+@section('content')
+    <div class="container d-flex justify-content-center bg-success">
+        <h1 class="display-1 fw-bold">testing API's</h1>
+    </div>
+@endsection
