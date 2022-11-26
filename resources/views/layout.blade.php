b2b7a74eb27a3a93a657f9a36405d5098e0f2e18
Mohamed_Yasser <mohamed.yasser99999@gmail.com>
2022-11-26 17:20:38 +0200

    updating logic

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" >
+
 </head>
 <body>
 @yield('content')
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,4 +6,7 @@
     <div class="container d-flex justify-content-center bg-success">
         <h1 class="display-1 fw-bold">testing API's</h1>
     </div>
+    <div class="content m-3">
+        <p> this is export of API</p>
+    </div>
 @endsection
