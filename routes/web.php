0296242a90bc5eb589320cd752679e80473fef42
Mohamed_Yasser <mohamed.yasser99999@gmail.com>
2022-11-28 01:03:12 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController ;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,5 +27,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/games' , [APIcontroller::class , 'index'])->name('api') ;
 
 require __DIR__.'/auth.php';
